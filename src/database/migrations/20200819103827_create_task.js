d991cd9316f528fe433b048bea85c823727cd8f4
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2020-08-19 10:50:31 -0300

    Creating Task Migration

---
+++ src/database/migrations/20200819103827_create_task.js
@@ -0,0 +1,18 @@
+exports.up = function ({ schema }) {
+  return schema.createTable("Task", function (table) {
+    table.increments("id").notNullable();
+
+    table.string("endPoint").notNullable();
+    table
+      .timestamp("created_at")
+      .notNullable()
+      .defaultTo(new Date().toUTCString());
+    table.timestamp("do_task").notNullable();
+    table.boolean("queued").notNullable().defaultTo(false);
+    table.boolean("done").notNullable().defaultTo(false);
+  });
+};
+
+exports.down = function ({ schema }) {
+  return schema.dropTable("Task");
+};
